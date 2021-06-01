3cf69b134f30a8ee727b47a208f2f3c7bc408af5
xifu.yy <daryl.yy@cainiao.com>
2021-06-01 20:03:29 +0800

    update

--- src/main/java/practice/tree/PathSum2.java
+++ src/main/java/practice/tree/PathSum2.java
@@ -8,14 +8,33 @@ import recursion.Model.TreeNode;
  * @date 2021/06/01 19:48
  */
 public class PathSum2 {
+
+    int cnt = 0;
+
     /**
      * https://leetcode-cn.com/problems/path-sum-iii/description/
+     *
      * @param root
      * @param sum
      * @return
      */
     public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        return pathSumWithRoot(root, sum) + pathSum(root.getLeft(), sum) + pathSum(root.getRight(), sum);
+    }
 
+    public int pathSumWithRoot(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        int res = 0;
+        if (root.getValue() == sum) {
+            res++;
+        }
+        return res + pathSumWithRoot(root.getLeft(), sum - root.getValue()) + pathSumWithRoot(root.getRight(),
+            sum - root.getValue());
     }
 
 }
