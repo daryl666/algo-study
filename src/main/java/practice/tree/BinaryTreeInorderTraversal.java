6a2ba0a314aa83ae7596870304555ab2ba2749f0
xifu.yy <daryl.yy@cainiao.com>
2021-06-02 20:18:32 +0800

    update

---
+++ src/main/java/practice/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,10 @@
+package practice.tree;
+
+/**
+ * @author xifu.yy
+ * @description
+ * @date 2021/06/02 20:18
+ */
+public class BinaryTreeInorderTraversal {
+
+}
--- src/main/java/practice/tree/BinaryTreePostorderTraversal.java
+++ src/main/java/practice/tree/BinaryTreePostorderTraversal.java
@@ -1,6 +1,9 @@
 package practice.tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 import recursion.Model.TreeNode;
 
@@ -10,8 +13,30 @@ import recursion.Model.TreeNode;
  * @date 2021/06/02 20:15
  */
 public class BinaryTreePostorderTraversal {
+    /**
+     * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+     * @param root
+     * @return
+     */
     public List<Integer> postorderTraversal(TreeNode root) {
-
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.getValue());
+            if (node.getLeft() != null) {
+                stack.push(node.getLeft());
+            }
+            if (node.getRight() != null) {
+                stack.push(node.getRight());
+            }
+        }
+        Collections.reverse(res);
+        return res;
     }
 
 }
